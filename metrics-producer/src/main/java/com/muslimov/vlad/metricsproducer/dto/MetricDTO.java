ff31195fae086b55ad0cf56057b1563d742e5a5d
Vlad Muslimov <muslimov.vlad@mail.ru>
2024-04-17 20:56:57 +0500

    Настройка Kafka Producer

---
+++ metrics-producer/src/main/java/com/muslimov/vlad/metricsproducer/dto/MetricDTO.java
@@ -0,0 +1,15 @@
+package com.muslimov.vlad.metricsproducer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MetricDTO {
+
+    private String name;
+    private String description;
+    private Double value;
+}
